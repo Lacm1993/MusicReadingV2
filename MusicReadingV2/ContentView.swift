86e7725f2b9678b84a220a78081e803dfa2b3634
Lacm1993 <lacm19930429@hotmail.com>
2022-11-09 13:50:01 -0600

    Initial Commit

---
+++ MusicReadingV2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MusicReadingV2
+//
+//  Created by Luis Alfonso Contreras Maya on 09/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicReadingV2/MusicReadingV2App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicReadingV2App.swift
+//  MusicReadingV2
+//
+//  Created by Luis Alfonso Contreras Maya on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicReadingV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
